e4c50f5892e47f65f68a7cc8bb009b2849019c47
icebreaker <mavrovski@gmail.com>
2018-03-28 20:49:01 +0300

    add

---
+++ JS Fundamentals - януари 2018/04Functions/03SquareOfStars.js
@@ -0,0 +1,12 @@
+function squareOfStars(n) {
+    function printStars(count = n) {
+        console.log("*" + " *".repeat(count - 1));
+    }
+
+    for (let i = 1; i <= n; i++) {
+        printStars();
+
+    }
+}
+
+squareOfStars(3);
