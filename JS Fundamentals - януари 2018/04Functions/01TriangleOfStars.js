07fbfc4721b237d7dd6cc9eef059a7a2a85014a4
icebreaker <mavrovski@gmail.com>
2018-03-28 20:35:52 +0300

    commit

--- JS Fundamentals - януари 2018/04Functions/01TriangleOfStars.js
+++ JS Fundamentals - януари 2018/04Functions/01TriangleOfStars.js
@@ -1,3 +1,16 @@
-function printTriangle() {
+function starsTriangle(size) {
+    for (let i = 1; i <= size; i++) {
+        print(i)
 
+    }
+    for (let i = size - 1; i > 0; i--) {
+        print(i)
+
+    }
+
+    function print(n) {
+        console.log("* ".repeat(n).trim())
+    }
 }
+
+starsTriangle(3);
