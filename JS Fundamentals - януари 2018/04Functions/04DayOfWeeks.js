ba7e388a1d4cb8b6bdd3ab63427cc7a4837d0d2b
icebreaker <mavrovski@gmail.com>
2018-03-29 00:51:20 +0300

    Day of weeks

---
+++ JS Fundamentals - януари 2018/04Functions/04DayOfWeeks.js
@@ -0,0 +1,8 @@
+function dayOfWeeks(day) {
+    let days = ['Monday','Tuesday','Wednesday','Thursday'
+        ,'Friday','Saturday','Sunday'];
+    let index = days.indexOf(day);
+    return index > -1 ? index +1 : 'error'
+}
+
+console.log(dayOfWeeks("Monday"));
