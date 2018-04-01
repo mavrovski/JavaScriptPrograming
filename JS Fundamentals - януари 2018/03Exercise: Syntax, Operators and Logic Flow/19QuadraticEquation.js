8e00141a71fb86e810c4a543d31cd750b1f5447a
icebreaker <mavrovski@gmail.com>
2018-04-01 10:15:14 +0300

    change task

--- JS Fundamentals - януари 2018/03Exercise: Syntax, Operators and Logic Flow/19QuadraticEquation.js
+++ JS Fundamentals - януари 2018/03Exercise: Syntax, Operators and Logic Flow/19QuadraticEquation.js
@@ -4,13 +4,13 @@ function quadraticEquation(a,b,c) {
     {
         x1 = (-b+Math.sqrt(discriminant))/(2*a)
         x2 = (-b-Math.sqrt(discriminant))/(2*a)
-        console.log(x1)
-        console.log(x2)
+        console.log(x1);
+        console.log(x2);
     }else if( discriminant === 0 ){
         x1= -b/(2*a)
         console.log(x1);
     }else {
-        console.log('No')
+        console.log('No');
     }
 }
 quadraticEquation(1,-12,36)
