b550f099c9b48fb03066153d758cf69c11f34c8c
icebreaker <mavrovski@gmail.com>
2018-04-01 21:41:39 +0300

    task

--- JS Fundamentals - януари 2018/06ArraysAndMatrices/07SmallestTwoNumbers.js
+++ JS Fundamentals - януари 2018/06ArraysAndMatrices/07SmallestTwoNumbers.js
@@ -1,6 +1,6 @@
-function smallestTwoNumbers(arr) {
+function 07(arr) {
     console.log(arr.sort((a,b) => a-b)
         .slice(0,2));
 }
 arr = [100,200,3000,4,5,6,1];
-smallestTwoNumbers(arr);
+07(arr);
