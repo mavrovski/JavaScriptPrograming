6638734c58fa8b0aec53b1eba26a00930ddee2e3
icebreaker <mavrovski@gmail.com>
2018-04-03 11:01:50 +0300

    task

---
+++ JS Fundamentals - януари 2018/08StringsAndRegExp/01PrintStringLetters.js
@@ -0,0 +1,8 @@
+function printStringLetters(str) {
+    for (let i = 0; i < str.length; i++) {
+        console.log(`str[${i}] -> ${str[i]}`)
+
+    }
+}
+let str = 'test';
+printStringLetters(str);
