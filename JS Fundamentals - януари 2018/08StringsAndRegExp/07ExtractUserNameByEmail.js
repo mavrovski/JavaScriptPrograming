b2a19bba431e877c3e35c928edac4ede0bd6f762
icebreaker <mavrovski@gmail.com>
2018-04-07 13:15:55 +0300

    task

---
+++ JS Fundamentals - януари 2018/08StringsAndRegExp/07ExtractUserNameByEmail.js
@@ -0,0 +1,17 @@
+function emails(arr) {
+    let finalResult = []
+    for (let i = 0; i < arr.length; i++) {
+        let token = arr[i].split('@')
+        let domains = token[1].split('.')
+        let result = token[0] + '.'
+        for (let str of domains) {
+            result += str[0]
+        }
+        finalResult.push(result)
+
+    }
+    console.log(finalResult.join(', '))
+}
+
+let arr = ['peshoo@gmail.com', 'todor_43@mail.dir.bg']
+emails(arr)
